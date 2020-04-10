54f94c5bd4abf59b1a6f0669561d2105398189dc
Akshay <akshay.kumar@tekfriday.com>
2020-04-10 12:26:20 +0530

    Landing page changes

--- static/js/index.js
+++ static/js/index.js
@@ -21,22 +21,13 @@ document.addEventListener('DOMContentLoaded', function () {
             },
 
             revenue: function () {
-                $("#loading").show();
-                $.ajax({
-                    url: "https://covid19.mathdro.id/api",
-                    method: "GET",
-                    dataType: "json"
-                }).done(function (response) {
-                    //this.add(response)
-                    this.cards(response)
-                    // google.charts.setOnLoadCallback(function () {
-                    //     this.trend(response)
-                    // }.bind(this));
-                    $("#loading").hide();
-                }.bind(this)).fail(function (error) {
-                    console.log(error);
-                    $("#loading").hide();
-                });
+              $.ajax({
+                url: 'https://api.thevirustracker.com/free-api?global=stats',
+                dataType: 'json',
+                success: function(data) {
+                 this.cards(data)
+                }.bind(this)
+              });
             },
             revenue1: function () {
                 $("#loading").show();
@@ -67,6 +58,15 @@ document.addEventListener('DOMContentLoaded', function () {
                     console.log(error);
                     $("#loading").hide();
                 });
+            },
+            revenue3: function () {
+              $.ajax({
+                url: 'https://api.thevirustracker.com/free-api?countryTotals=ALL',
+                dataType: 'json',
+                success: function(data) {
+                  this.add(data);
+                }.bind(this)
+              });
             },
             dropdown:function(box){
                 var countries=[];
@@ -155,7 +155,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     new_arr1.push(daily[i].deaths.total)
                     new_arr2.push(daily[i].totalConfirmed)
                 }
-                this.lineChart(new_arr,new_arr2)
+                this.lineChart(new_arr,new_arr1)
                 var urChart = new Chart(ctx_1, {
                     type: "line",
                     data: {
@@ -224,106 +224,61 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             
             cards: function (data) {
-                var dateObj = new Date();
-                var month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
-                var date = ('0' + (dateObj.getDate() - 1)).slice(-2);
-                var year = dateObj.getFullYear();
-                var shortDate = month + '-' + date + '-' + year;
-                // console.log(shortDate)
-                // var obj = JSON.parse(data);
-                $("#count0").html(this.formatNumber(data.confirmed.value))
-                $("#count1").html(this.formatNumber(data.recovered.value))
-                $("#count2").html(this.formatNumber(data.deaths.value))
-            }
-            // bass: function (a, b, c, d) {
-            //     var tltp = "<strong>" + "App_Date:" + a + "</strong>" + "<br>" + "<strong>" + "CAD:" + b + "</strong>" + "<br>" + "<strong>" + "Loans:" + c + "</strong>" + "<br>" + "<strong>" + "Opened:" + d + "</strong>" + "<br>";
-            //     return tltp;
-            // },
-            // trend: function (r_data) {
-            //     date = [], count = [], pay = [],withdraw=[];
-            //     for (var i = 0; i < r_data.length; i++) {
-            //         date.push(r_data[i].App_Date)
-            //         count.push(r_data[i].CAD)
-            //         pay.push(r_data[i].Loans)
-            //         withdraw.push(r_data[i].Opened)
-            //     }
-            //     date.reverse();
-            //     count.reverse();
-            //     pay.reverse();
-            //     withdraw.reverse();
-            //     var container = document.getElementById('linechart');
-            //     var chart = new google.visualization.LineChart(container);
-            //     var dataTable = new google.visualization.DataTable();
-            //     dataTable.addColumn('string', 'Date');
-            //     dataTable.addColumn('number', 'CAD');
-            //     dataTable.addColumn({
-            //         type: 'string',
-            //         role: 'tooltip',
-            //         p: {
-            //             html: true
-            //         }
-            //     });
-            //     var accounts = [];
-            //     for (i = 0; i < count.length; i++) {
-            //         accounts.push([date[i], count[i], this.bass(date[i], count[i], pay[i],withdraw[i])]);
-            //     }
-            //     dataTable.addRows(accounts);
-            //     var chartHeight = '550';
-            //     var options = {
-            //          is3D: true,
-            //         tooltip: {
-            //                 isHtml: true
-            //             },
-            //         title: 'Tracker',
-            //         hAxis: {title: 'Date',  titleTextStyle: {color: '#333'},
-            //         slantedText:true, slantedTextAngle:40},
-            //         explorer: {actions: ["dragToZoom", "rightClickToReset"]},
-            //         legend: {position: "top"},
-            //           pointSize: 10,
-            //           pointShape: { type: 'triangle', rotation: 180 },
-            //         vAxis: {
-            //           title: 'CAD'
-            //         },
-            //         height: chartHeight,
-            //                 width: '100%',
-            //       };
+                // var dateObj = new Date();
+                // var month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
+                // var date = ('0' + (dateObj.getDate() - 1)).slice(-2);
+                // var year = dateObj.getFullYear();
+                // var shortDate = month + '-' + date + '-' + year;
+                // // var obj = JSON.parse(data);
+                $("#count0").html(this.formatNumber(data.results[0].total_cases))
+                $("#count1").html(this.formatNumber(data.results[0].total_recovered))
+                $("#count2").html(this.formatNumber(data.results[0].total_deaths))
+                $("#count3").html(this.formatNumber(data.results[0].total_active_cases))
+                $("#count4").html(this.formatNumber(data.results[0].total_new_cases_today))
+                $("#count5").html(this.formatNumber(data.results[0].total_new_deaths_today))
+                $("#count6").html(this.formatNumber(data.results[0].total_serious_cases))
+                $("#count7").html(this.formatNumber(data.results[0].total_affected_countries))
 
 
-            //     chart.draw(dataTable, options);
-            // },
-            // add: function (table_data) {
-            //     let table = document.querySelector("table");
-            //     let data = Object.keys(table_data[0]);
-            //     this.generateTableHead(table, data);
-            //     this.generateTable(table, table_data);
-            // },
-            // generateTable: function (table, data) {
-            //     for (let element of data) {
-            //         let row = table.insertRow();
-            //         for (key in element) {
-            //             let cell = row.insertCell();
-            //             let text;
-            //             text = document.createTextNode(element[key]);
-            //             cell.appendChild(text);
-            //         }
-            //     }
-            // },
-            // generateTableHead: function (table, data) {
-            //     let thead = table.createTHead();
-            //     let row = thead.insertRow();
-            //     for (let key of data) {
-            //         let th = document.createElement("th");
-            //         th.className = 'thead';
-            //         let text = document.createTextNode(key);
-            //         th.appendChild(text);
-            //         row.appendChild(th);
-            //     }
-            // }
+            },
+            add: function (table_data) {
+             var c=table_data.countryitems[0];
+              console.log(c)
+              const dateRangeData = Object.keys(c).map(key => c[key]);
+              console.log(dateRangeData)
+                let table = document.querySelector("table");
+                let data = Object.keys(dateRangeData[0]);
+                this.generateTableHead(table, data);
+                this.generateTable(table, dateRangeData);
+            },
+            generateTable: function (table, data) {
+                for (let element of data) {
+                    let row = table.insertRow();
+                    for (key in element) {
+                        let cell = row.insertCell();
+                        let text;
+                        text = document.createTextNode(element[key]);
+                        cell.appendChild(text);
+                    }
+                }
+            },
+            generateTableHead: function (table, data) {
+                let thead = table.createTHead();
+                let row = thead.insertRow();
+                for (let key of data) {
+                    let th = document.createElement("th");
+                    th.className = 'thead';
+                    let text = document.createTextNode(key);
+                    th.appendChild(text);
+                    row.appendChild(th);
+                }
+            },
         },
         created: function () {
             this.revenue();
             this.revenue1();
             this.revenue2();
+            this.revenue3();
         },
         beforeCreate() {
         }
